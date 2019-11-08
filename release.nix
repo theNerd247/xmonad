b04156dbbffeaeb676cd6dbaae410881611fb5b5
theNerd247 <noah.harvey247 gm>
2019-11-07 22:00:15 -0500

    finished new release.nix

--- release.nix
+++ release.nix
@@ -1,15 +1,27 @@
-{ ... }: 
+{ pkgs, lib, ... }:
 
+let
+  xrandr = pkgs.fetchzip
+    { url = https://github.com/theNerd247/xmonad-contrib-xrandr/archive/v0.3.0-apha.2.tar.gz;
+      sha256 = "1n0vqh6frapycwb7qc23182q7wfl83fc1d2pbm1biqax1qz5nva6";
+    };
+in
 {
-  nixpkgs.config = (import /home/noah/src/com/xrandr/release.nix);
-
   services.xserver.windowManager = 
   {
     default = "xmonad";
     xmonad = {
+      config = ./xmonad.hs;
       enable = true;
       enableContribAndExtras = true;
       extraPackages = (hpkgs: [ hpkgs.xrandr ]);
+      haskellPackages = pkgs.haskellPackages.override 
+        {
+          overrides = new: old:
+          {
+            xrandr = new.callPackage xrandr {};
+          };
+        };
     };
   };
 }
